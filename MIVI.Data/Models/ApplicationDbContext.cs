bc229d1c78a4f2f2e4a9b4e5ad64c4a7b12d9d80
Siddharth <siddharth.singh0701@gmail.com>
2022-07-20 19:45:58 +0530

    Initial commit

---
+++ MIVI.Data/Models/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MIVI.Data.Models;
+
+namespace MIVI.Data.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        string connectionString = "Server=SIDDHARTH-SINGH;Database=miviecomm; integrated security=SSPI;trustservercertificate=True;MultipleActiveResultSets=true";
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+
+    }
+}
